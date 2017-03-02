b1cdd5f38ef1597112067867f381b46277ac7d8f
Jin Qian <jinqian@google.com>
2017-03-02 13:32:59 -0800

    ANDROID: sched: add a counter to track fsync
    Change-Id: I6c138de5b2332eea70f57e098134d1d141247b3f
    Signed-off-by: Jin Qian <jinqian@google.com>
    Git-commit: fa6406a95d9bc04f90406e1127308b2494456970
    Git-repo: https://android.googlesource.com/kernel/common.git
    Signed-off-by: Srinivasarao P <spathi@codeaurora.org>
    Signed-off-by: vlw <vlwwwwww@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2917,6 +2917,11 @@ static inline void inc_syscw(struct task_struct *tsk)
 {
 	tsk->ioac.syscw++;
 }
+
+static inline void inc_syscfs(struct task_struct *tsk)
+{
+	tsk->ioac.syscfs++;
+}
 #else
 static inline void add_rchar(struct task_struct *tsk, ssize_t amt)
 {
@@ -2931,6 +2936,9 @@ static inline void inc_syscr(struct task_struct *tsk)
 }
 
 static inline void inc_syscw(struct task_struct *tsk)
+{
+}
+static inline void inc_syscfs(struct task_struct *tsk)
 {
 }
 #endif
